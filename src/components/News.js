4ab89e20a45bd6ac1139c2e50f90df61bca4c97b
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-10-03 19:07:43 +0530

    added News and News Item Component

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-export default function News() {
-  return (
-    <>
-     <div>This is a news component</div>
-     <NewsItem/>
-     <NewsItem/>
-     <NewsItem/>
-     <NewsItem/>
-     <NewsItem/>
-    </>
-  )
+export default class News extends Component {
+  render() {
+    return (
+        <>
+        <div className="container">
+                  
+                <h1>NewsMonkey - Top HeadLines :</h1>
+                
+                <div className="row">
+
+                    <div className="col-md-1">  <NewsItem title="mytitle1" description="myDescription1"/> </div>
+                    <div className="col-md-1">  <NewsItem title="mytitle2" description="myDescription2"/> </div>
+                    <div className="col-md-1">  <NewsItem title="mytitle3" description="myDescription3"/> </div>
+                    <div className="col-md-1">  <NewsItem title="mytitle4" description="myDescription4"/> </div>
+                    <div className="col-md-1">  <NewsItem title="mytitle5" description="myDescription5"/> </div>
+                
+                </div>
+            
+
+        </div>
+        
+       
+        </>
+    )
+  }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function NewsItem() {
-  return (
-    <div>NewsItem component </div>
-  )
+export default class NewsItem extends Component {
+  render() {
+
+    let {title,description} = this.props;  // Object Destructuring Syntax [Note : We can also use this.props.prop_name :)]
+    return (
+        <div className="newsItem">
+
+            <div className="card" style={{width: '18rem'}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+
+      </div>
+    )
+  }
 }
